dde76a1c797a9f522b84e92227c885c6e1f305c1
Quantum <killinggamer4mc@gmail.com>
2018-11-18 22:03:10 +0000

    Added 'successfull startup' signal on successfull launch

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const Discord = require("discord.js-commando");
 const Client = new Discord.Client();
 const clientconfig = require("./clientconfig.json");
 
+Client.on ('ready', async () => {
+  console.log(`${bot.user.username} is online`);
+});
+
 Client.login(clientconfig.token);
